1bf21045205c5c26407d67a51bcf81eb8fd1f13f
Katye Stevens <katye333@gmail.com>
2020-04-06 18:17:59 -0400

    feat: Created doughnut charts for each job

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 import { Person, Apps, Email, GitHub, LinkedIn, PhoneAndroid, ImportantDevices } from '@material-ui/icons';
 import { Card } from '@material-ui/core';
 import ParticleComponent from './ParticleComponent';
+import PieChart from 'react-minimal-pie-chart';
 
 function App() {
+
     return (
         <div className="App" style={{ display: 'flex', flex: 1, flexDirection: 'column', alignItems: 'center', position: 'absolute', top: 0, left: 0 }}>
             <ParticleComponent />
@@ -90,7 +92,7 @@ function App() {
                             </div>
 
                             <div style={{ display: 'flex', flexDirection: 'row' }}>
-                                <ul style={{ width: 325 }}>
+                                <ul style={{ width: 350, minHeight: 250 }}>
                                     <li>Developed a React Native application for use on the respective app stores</li>
                                     <li>Integrated technologies such as Firebase for notifications and analytics, Square Reader for credit card
                                         processing, and PSPDFKit for generating, annotating and signing PDFs</li>
@@ -99,9 +101,52 @@ function App() {
                                     <li>Followed the Material UI guidelines set by Google to create a truly beautiful app</li>
                                 </ul>
                             </div>
+
+                            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+                                <PieChart
+                                    animate={false}
+                                    animationDuration={500}
+                                    animationEasing="ease-out"
+                                    cx={50}
+                                    cy={50}
+                                    data={[
+                                        {
+                                            color: '#43A19E',
+                                            title: 'React Native',
+                                            value: 50
+                                        },
+                                        {
+                                            color: '#7B43A1',
+                                            title: 'Android',
+                                            value: 30
+                                        },
+                                        {
+                                            color: '#F2317A',
+                                            title: 'iOS',
+                                            value: 20
+                                        }
+                                    ]}
+                                    label={({ data, dataIndex }) => data[dataIndex].title}
+                                    labelPosition={60}
+                                    labelStyle={{
+                                        fontFamily: 'sans-serif',
+                                        fontSize: '5px'
+                                    }}
+                                    lengthAngle={360}
+                                    lineWidth={20}
+                                    paddingAngle={18}
+                                    radius={50}
+                                    rounded
+                                    startAngle={0}
+                                    viewBoxSize={[
+                                        100,
+                                        100
+                                    ]}
+                                />
+                            </div>
                         </div>
 
-                        <div style={{ width: 1, height: 400, borderColor: '#cccccc', backgroundColor: '#cccccc' }} />
+                        <div style={{ width: 1, height: 750, borderColor: '#cccccc', backgroundColor: '#cccccc' }} />
 
                         <div style={{ display: 'flex', flexDirection: 'column', marginLeft: 15, flex: 0.5 }}>
                             <div style={{ display: 'flex', flexDirection: 'row' }}>
@@ -116,13 +161,66 @@ function App() {
                             </div>
 
                             <div style={{ display: 'flex', flexDirection: 'row' }}>
-                                <ul style={{ width: 325 }}>
+                                <ul style={{ width: 350, minHeight: 250 }}>
                                     <li>Developed an web application using AppCache and Service Workers in conjunction with C# to facilitate offline capabilities</li>
                                     <li>Solely responsible for changes to the application interface using Bootstrap, Flexbox and custom CSS</li>
                                     <li>Created stored procedures using Sybase and MSSQL</li>
                                     <li>Configured Jest with Enzyme to facilitate testing throughout the development process</li>
                                     <li>Implemented GPS Geotracking using a REST web api to let businesses monitor employee and equipment movement on the job site.</li>
                                 </ul>
+                            </div>
+
+                            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+                                <PieChart
+                                    animate={false}
+                                    animationDuration={500}
+                                    animationEasing="ease-out"
+                                    cx={50}
+                                    cy={50}
+                                    data={[
+                                        {
+                                            color: '#43A19E',
+                                            title: '.NET/#C',
+                                            value: 20
+                                        },
+                                        {
+                                            color: '#7B43A1',
+                                            title: 'SQL',
+                                            value: 10
+                                        },
+                                        {
+                                            color: '#F2317A',
+                                            title: 'HTML5',
+                                            value: 20
+                                        },
+                                        {
+                                            color: '#FF9824',
+                                            title: 'CSS3',
+                                            value: 10
+                                        },
+                                        {
+                                            color: '#58CF6C',
+                                            title: 'JavaScript',
+                                            value: 40
+                                        }
+                                    ]}
+                                    label={({ data, dataIndex }) => data[dataIndex].title}
+                                    labelPosition={70}
+                                    labelStyle={{
+                                        fontFamily: 'sans-serif',
+                                        fontSize: '5px'
+                                    }}
+                                    lengthAngle={360}
+                                    lineWidth={20}
+                                    paddingAngle={18}
+                                    radius={50}
+                                    rounded
+                                    startAngle={0}
+                                    viewBoxSize={[
+                                        100,
+                                        100
+                                    ]}
+                                />
                             </div>
                         </div>
                     </div>
