2aa07c6e23550b2b3b03eecd1ef33974b19e48ad
Katye Stevens <katye333@gmail.com>
2020-04-22 11:07:47 -0400

    feat: Coming soon page

---
+++ src/Projects.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Projects() {
+	return (
+		<div style={{ display: 'flex', flexDirection: 'column', flex: 1, alignItems: 'center' }}>
+			<p style={{ fontSize: 24, fontWeight: '700' }}>Coming soon</p>
+			<p style={{ fontSize: 18 }}>In the meantime, check out my&nbsp;
+				<a 
+	                href="https://github.com/katye333"
+	                target="_blank"
+	                rel="noopener noreferrer"
+	                style={{  }}>
+	                Github
+	            </a>
+	        </p>    
+		</div>
+	)
+}
+
+export default Projects;
